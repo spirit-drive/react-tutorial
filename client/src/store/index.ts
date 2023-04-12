2a6d8c65cc7d2f9123b8df81e88a6e28c31882ce
Igor <spirit-drive@yandex.ru>
2023-04-12 19:52:50 +0300

    refactor(src/store): remove unnecessary

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,14 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import createSagaMiddleware from 'redux-saga'
-import inited from './reducers/inited';
 import rootSaga from './sagas';
 
 const sagaMiddleware = createSagaMiddleware()
 
 export const store = configureStore({
-  reducer: {
-    inited,
-  },
+  reducer: {},
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware),
 })
 
