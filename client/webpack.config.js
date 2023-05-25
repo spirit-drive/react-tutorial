5762badf831443ecea4848bc99765cf7fe8ec2e1
igor <spirit-drive@yandex.ru>
2023-05-26 00:23:13 +0300

    feat: made favicon

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -74,6 +74,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+      favicon: "favicon.ico"
     }),
     new ContextReplacementPlugin(/moment[/\\]locale$/, /ru|en/),
     new CleanWebpackPlugin(),
