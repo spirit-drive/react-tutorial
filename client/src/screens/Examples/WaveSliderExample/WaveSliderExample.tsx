8b7c6bb0ecf885dc09e9991c0031b0889a32422d
igor <spirit-drive@yandex.ru>
2023-05-28 20:13:09 +0300

    chore(src/screens/Examples/WaveSliderExample): improve a look

--- client/src/screens/Examples/WaveSliderExample/WaveSliderExample.tsx
+++ client/src/screens/Examples/WaveSliderExample/WaveSliderExample.tsx
@@ -7,28 +7,22 @@ export type ModalExampleProps = {
   className?: string;
 };
 
+const items = [
+  'https://www.nastol.com.ua/pic/201404/2560x1600/nastol.com.ua-92859.jpg',
+  'https://img2.goodfon.ru/original/2048x1365/2/92/priroda-nebo-oblaka-ozero.jpg',
+  'https://arte1.ru/images/detailed/4/23377.jpg',
+  'https://img1.goodfon.com/original/1024x768/9/87/priroda-gory-leto-svet-solnca.jpg',
+  'https://cojo.ru/wp-content/uploads/2022/12/krasnoe-ozero-v-kanade-1-1.webp',
+];
+
 export const WaveSliderExample: FC<ModalExampleProps> = ({ className }) => (
   <div className={cn(s.root, className)}>
-    <WaveSlider>
-      <div
-        className={s.item}
-        style={{
-          backgroundImage: 'url(https://www.nastol.com.ua/pic/201404/2560x1600/nastol.com.ua-92859.jpg)',
-        }}
-      >
-        1
-      </div>
-      <div
-        className={s.item}
-        style={{
-          backgroundImage: 'url(https://img2.goodfon.ru/original/2048x1365/2/92/priroda-nebo-oblaka-ozero.jpg)',
-        }}
-      >
-        2
-      </div>
-      <div className={s.item} style={{ backgroundImage: 'url(https://arte1.ru/images/detailed/4/23377.jpg)' }}>
-        3
-      </div>
+    <WaveSlider transitionDuration={1500}>
+      {items.map((item, i) => (
+        <div className={s.item} key={item} style={{ backgroundImage: `url(${item})` }}>
+          {i}
+        </div>
+      ))}
     </WaveSlider>
   </div>
 );
