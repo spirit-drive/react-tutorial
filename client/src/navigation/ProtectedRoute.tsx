a80ee249e10cb61dee447c4cb1f0986f6d501833
igor <spirit-drive@yandex.ru>
2023-06-06 21:21:42 +0300

    improve ProtectedRoute

--- client/src/navigation/ProtectedRoute.tsx
+++ client/src/navigation/ProtectedRoute.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC } from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate, useLocation } from 'react-router-dom';
 import { tokenSelectors } from 'src/store/token';
@@ -8,6 +8,6 @@ import { NavigationState } from './types';
 export const ProtectedRoute: FC<{ children: React.ReactNode }> = ({ children }) => {
   const token = useSelector<RootState, RootState['token']>(tokenSelectors.get);
   const location = useLocation();
-  if (token) return children as ReactElement;
+  if (token) return <>{children}</>;
   return <Navigate to="/auth" state={{ from: location } as NavigationState} replace />;
 };
