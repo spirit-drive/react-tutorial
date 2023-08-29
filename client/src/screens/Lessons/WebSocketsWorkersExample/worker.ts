f419f8932f041151222ec6f0c998c6416dd9f955
Igor Zvyagin <spirit-drive@yandex.ru>
2023-08-29 18:32:49 +0300

    feat(src/screens/Lessons/WebSocketsWorkersExample/WebWorkersExample): improve naming

--- client/src/screens/Lessons/WebSocketsWorkersExample/worker.ts
+++ client/src/screens/Lessons/WebSocketsWorkersExample/worker.ts
@@ -1,9 +1,9 @@
 import { compose } from 'src/utils/compose';
 import { getArray, permute } from './helpers';
 
-const handler = compose(permute, getArray);
+const handle = compose(permute, getArray);
 
 onmessage = function (e) {
   const { value } = e.data as { value: number };
-  postMessage([...handler(value)]);
+  postMessage([...handle(value)]);
 };
