2eca5eb176372fff3c590dfa5d5b2bbc045878a6
Igor Zvyagin <spirit-drive@yandex.ru>
2023-08-29 18:29:36 +0300

    feat(src/screens/Lessons/WebSocketsWorkersExample/WebWorkersExample): remove self

--- client/src/screens/Lessons/WebSocketsWorkersExample/worker.ts
+++ client/src/screens/Lessons/WebSocketsWorkersExample/worker.ts
@@ -1,10 +1,9 @@
-/* eslint-disable no-restricted-globals */
 import { compose } from 'src/utils/compose';
 import { getArray, permute } from './helpers';
 
 const handler = compose(permute, getArray);
 
-self.addEventListener('message', (e) => {
+onmessage = function (e) {
   const { value } = e.data as { value: number };
-  self.postMessage([...handler(value)]);
-});
+  postMessage([...handler(value)]);
+};
