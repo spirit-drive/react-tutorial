507e0d95482d1ec74482cdc865f1b88abe34faca
Igor Zvyagin <spirit-drive@yandex.ru>
2023-08-29 17:41:52 +0300

    feat(src/screens/Lessons/WebSocketsWorkersExample/socket): made simple example

--- client/src/screens/Lessons/WebSocketsWorkersExample/socket.ts
+++ client/src/screens/Lessons/WebSocketsWorkersExample/socket.ts
@@ -2,6 +2,4 @@
 import { io } from 'socket.io-client';
 import { HOST, SOCKET_PORT } from 'src/client/config';
 
-export const socket = io(`ws://${HOST}:${SOCKET_PORT}`, {
-  reconnectionDelayMax: 10000,
-});
+export const socket = io(`ws://${HOST}:${SOCKET_PORT}`);
